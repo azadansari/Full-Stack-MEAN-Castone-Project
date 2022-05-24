29afec06a4e3eee207065c86da7bd9623eac7650
azad ansari <azadansari7277@gmail.com>
2022-05-24 23:40:01 +0530

    update on homepage

--- Grocers/src/app/app.module.ts
+++ Grocers/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { WhyUsComponent } from './why-us/why-us.component';
+import { ProductCategorySliderComponent } from './product-category-slider/product-category-slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    WhyUsComponent,
+    ProductCategorySliderComponent
   ],
   imports: [
     BrowserModule,
